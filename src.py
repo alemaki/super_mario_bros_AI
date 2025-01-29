061b6ac410adfe250a6da75b51ac99895cf2ead6
Alexander Makedonski <alemak2002@gmail.com>
2025-01-29 22:20:04 +0200

    Added time limit to mario's life.

--- src.py
+++ src.py
@@ -3,15 +3,22 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
+
+# endviromental variables 
+
 #needs python 3.8
 done = True
-printed = False
+printed = True 
 for step in range(5000):
     if done:
         state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    if not printed:
-        break
+    state, reward, done, _, info = env.step(env.action_space.sample())
     env.render()
+
+    if not printed:
+        print(f"{state}, {type(state)}, {state.shape}, {state.size}, {reward}, {done}, {info}"
+        )
+        printed = True
+        break
 
 env.close()
