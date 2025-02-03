369d5962566faa87682bf5848718a4e6e4e41416
Alexander Makedonski <alemak2002@gmail.com>
2025-02-03 16:13:51 +0200

    Added actor-critic module.

---
+++ src/actor_critic/actor_critic.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+class ActorCritic(nn.Module):
+    def __init__(self, input_shape, n_actions: int, use_leaky_relu: bool = False, channel_multiplier: int = 1):
+        super(ActorCritic, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32*channel_multiplier, kernel_size=8, stride=4),
+            nn.LeakyReLU if use_leaky_relu else nn.ReLU,
+            nn.Conv2d(32*channel_multiplier, 64*channel_multiplier, kernel_size=4, stride=2),
+            nn.LeakyReLU if use_leaky_relu else nn.ReLU,
+            nn.Conv2d(64*channel_multiplier, 64*channel_multiplier, kernel_size=3, stride=1),
+            nn.LeakyReLU if use_leaky_relu else nn.ReLU
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(self._feature_size(input_shape), 512),
+            nn.LeakyReLU if use_leaky_relu else nn.ReLU
+        )
+
+        self.actor = nn.Linear(512*channel_multiplier, n_actions)
+        self.critic = nn.Linear(512*channel_multiplier, 1)
+
+    def _feature_size(self, shape):
+        with torch.no_grad():
+            return self.conv(torch.zeros(1, *shape)).view(1, -1).size(1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x.view(x.size(0), -1))
+        return self.actor(x), self.critic(x)
